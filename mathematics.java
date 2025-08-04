1f129ae914bf99bf9dd47e94ca0aacb5055aa2a2
Ayaan-DSA <ayaankhanak3589@gmail.com>
2025-08-04 14:48:32 +0530

    math class 1

---
+++ mathematics.java
@@ -0,0 +1,23 @@
+
+
+public class mathematics {
+    public static void main(String[] args) {
+        System.out.println(Math.PI);
+        System.out.println(Math.E);
+        double result;
+        result = Math.pow(2, 3);
+        double result1 = Math.abs(-5);
+        double result2 = Math.sqrt(25);
+        double result3 = Math.round(3.14);
+        double result4 = Math.ceil(5.23);
+        double result5 = Math.floor(2.98);
+        double result6 = Math.max(25 , 89);
+        System.out.println(result);
+            System.out.println(result1);
+                System.out.println(result2);
+                    System.out.println(result3);
+                        System.out.println(result4);
+                            System.out.println(result5);
+                                System.out.println(result6);
+    }
+}
